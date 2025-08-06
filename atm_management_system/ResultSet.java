c69a360af1635eabc6e2dbe6f8beb9643bf3d7d8
Pradip-Garai <pradipgarai173@gmail.com>
2025-08-06 09:36:51 +0530

    first commit

---
+++ atm_management_system/ResultSet.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atm_management_system;
+
+/**
+ *
+ * @author Pradip Garai
+ */
+class ResultSet {
+
+    boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    String getString(String balance) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
